ef556df9da076a46431d8a236f7ebfea3af9c79f
prousitbat <jennifer.proust@gmail.com>
2025-11-28 19:41:36 +0100

    feat(add-form): add form with react-hook-form
    Also improve modal design and behaviour

--- src/components/CreateStepForm.tsx
+++ src/components/CreateStepForm.tsx
@@ -1,12 +1,98 @@
 import {Card} from "./ui";
+import { useForm } from "react-hook-form";
 
-type CreateStepFormProps = {};
+type CreateStepFormProps = {
+  formId: string
+  onSaved: ()=>void;
+};
 
-const CreateStepForm = ({}: CreateStepFormProps) => {
+const CreateStepForm = ({formId, onSaved}: CreateStepFormProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    defaultValues: {
+      label: "",
+      type: "",
+      content: ""
+    }
+  });
+    
   return (
     <Card className="mb-2">
-      <h1 className="text-2xl uppercase">Add a new step</h1>
-      <div>Here should be the form</div>
+      <h1 className="text-2xl uppercase mb-4">Add a new step</h1>
+      <form
+        id={formId}
+        onSubmit={handleSubmit((data) => {
+          alert(JSON.stringify(data));
+          onSaved();
+        })}
+        className="px-8 pt-6 pb-8 mb-4"
+      >
+        <div className="mb-8">
+          <label
+            htmlFor="label"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Label
+          </label>
+          <input
+            id="label"
+            type="text"
+            {...register(
+              "label",
+              {required: true}
+            )}
+            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline 
+                ${errors.label && "shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"}`
+            }
+          />
+          {errors.label && <p className="text-red-500 text-xs italic">Please choose a label for the step</p>}
+        </div>
+
+        <div className="mb-8">
+          <label
+            htmlFor="type"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Type
+          </label>
+          <input
+            id="type"
+            type="text"
+            placeholder={`"question" / "message" / "decision"`}
+            {...register(
+              "type",
+              {required: true}
+            )}
+            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline 
+            ${errors.type && "shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"}`
+            }
+          />
+          {errors.type && <p className="text-red-500 text-xs italic">Please choose a type for your step</p>}
+        </div>
+
+        <div className="mb-8">
+          <label
+            htmlFor="content"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Content
+          </label>
+          <textarea
+            id="content"
+            {...register(
+              "content",
+              {required: true}
+            )}
+            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-[4px]
+            ${errors.content && "shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"}`
+            }
+          />
+          {errors.content && <p className="text-red-500 text-xs italic">Please define a content</p>}
+        </div>
+      </form>
     </Card>
   );
 
--- src/components/ui/ModalDialog.tsx
+++ src/components/ui/ModalDialog.tsx
@@ -1,30 +1,57 @@
 import {Button} from "./Button.tsx";
+import {useEffect} from "react";
 
 type ModalContentProps = React.HTMLAttributes<HTMLDivElement> & {
   onClose: () => void;
   onSave: () => void;
+  formId?: string | null;
 };
-
-export default function ModalDialog({ onClose, onSave, className, children, ...props }: ModalContentProps) {
+export default function ModalDialog({ onClose, onSave, className, children, formId = null, ...props }: ModalContentProps) {
   const classes = `
         shadow-[0_7px_29px_rgba(100,100,111,0.3)]
-        bg-white
-        border-solid
-        border-[2px]
-        rounded-[12px]
         absolute
-        size-[var(--modal-width)]
-        top-[calc(var(--modal-margin)/2)]
-        left-[calc(var(--modal-margin)/2)] 
+        size-full
+        top-0
+        bottom-0 
         p-4
+        sm:p-10
+        md:p-20
+        lg:p-30
+        overflow-scroll
+        bg-black
+        bg-opacity-25
+        flex
+        flex-col
+        justify-center
+        items-center
         ${className}
     `;
+  useEffect(() => {
+    // Disables Background Scrolling whilst the SideDrawer/Modal is open
+    if (typeof window != 'undefined' && window.document) {
+      document.body.style.overflow = 'hidden';
+    }
+
+    return () => {
+      // Unsets Background Scrolling to use when SideDrawer/Modal is closed
+      document.body.style.overflow = 'unset';
+    };
+  }, []);
   return (
     <div className={classes} {...props}>
-      {children}
-      <div className="flex justify-end gap-3">
-        <Button variant="secondary" onClick={onClose}>Cancel</Button>
-        <Button onClick={onSave}>Save</Button></div>
+      <div className="overflow-scroll max-w-3xl w-full">
+        {children}
+        <div className="flex justify-center gap-3">
+          <Button variant="secondary" onClick={onClose}>Cancel</Button>
+          <Button
+            onClick={onSave}
+            type={formId ? 'submit' : 'button'}
+            {...(formId && ({ form: formId }))}
+          >
+            Save
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
